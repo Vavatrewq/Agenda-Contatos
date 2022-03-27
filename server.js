7046c00dafe803540249edaac6c81883c98c19a8
Wagner <wagnerw651@gmail.com>
2022-03-27 15:23:16 -0300

    minor-agenda

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.use(routes);
 
 app.on('pronto', () => {
   app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log("connect-agenda...");
   });
 });
