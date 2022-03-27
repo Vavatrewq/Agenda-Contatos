9d0c32a37af42625544c7c7818274b163c7b4f6f
Wagner <wagnerw651@gmail.com>
2022-03-27 12:31:09 -0300

    config-agenda:07

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-// const port = process.env.PORT || 3000
 const mongoose = require('mongoose');
 mongoose.connect(process.env.CONNECTIONSTRING,
   {
